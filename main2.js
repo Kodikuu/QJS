c21f12f91deab6d9ee046eae5fad2ac172c2673a
Kodikuu <Kodikuu@users.noreply.github.com>
2021-04-09 18:25:18 +0100

    Syntax example

--- main2.js
+++ main2.js
@@ -1,4 +1,12 @@
 
 import * as libc from 'libc';
+import * as mty from 'mty';
 
-libc.print("Yes!")
+libc.print(`mty.MTY_KEY_MAX is ${mty.MTY_KEY_MAX}`)
+
+libc.print(`mty.MTY_DrawData.displaySize.x == ${mty.MTY_DrawData.displaySize.x}`)
+
+libc.print(`mty.MTY_Event.text[1] == ${mty.MTY_Event.text[1]}`)
+libc.print(`mty.MTY_Event.text[2] == ${mty.MTY_Event.text[2]}`)
+
+libc.print(`mty.MTY_ControllerEvent.axes[0].value == ${mty.MTY_ControllerEvent.axes[0].value}`)
