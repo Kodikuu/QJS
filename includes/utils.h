e87f3d7a8aad7abb6e4b303cc934fc5fbe954b03
Kodikuu <Kodikuu@users.noreply.github.com>
2021-04-12 16:34:02 +0100

    Fix signatures

--- includes/utils.h
+++ includes/utils.h
@@ -1,9 +1,9 @@
 #include "quickjs.h"
 
-static int JSToInt32(JSContext *ctx, JSValueConst val);
+static int32_t JSToInt32(JSContext *ctx, JSValueConst val);
 
-static int JSToInt64(JSContext *ctx, JSValueConst val);
+static int64_t JSToInt64(JSContext *ctx, JSValueConst val);
 
-static int JSToInt64Ext(JSContext *ctx, JSValueConst val);
+static int64_t JSToInt64Ext(JSContext *ctx, JSValueConst val);
 
-static int JSToFloat64(JSContext *ctx, JSValueConst val);
+static double JSToFloat64(JSContext *ctx, JSValueConst val);
