21eb0b6be1ac217619331fb4c653ce41ace92138
Kodikuu <Kodikuu@users.noreply.github.com>
2021-04-16 16:17:01 +0100

    Smarter about utils

--- includes/utils.h
+++ includes/utils.h
@@ -4,10 +4,4 @@ static int32_t JSToInt32(JSContext *ctx, JSValueConst val);
 
 static int64_t JSToInt64(JSContext *ctx, JSValueConst val);
 
-static int64_t JSToInt64Ext(JSContext *ctx, JSValueConst val);
-
 static double JSToFloat64(JSContext *ctx, JSValueConst val);
-
-#define JSToChar JSToInt32
-
-#define JSToSizeT JSToInt32
